ee020957fd5bfe50c20114f8b41262b89a3840a2
Lucas Meijer <lucas@unity3d.com>
2011-03-08 09:55:37 +0100

    attempt 1 at linux runtime

--- build_runtime_linux.pl
+++ build_runtime_linux.pl
@@ -1,10 +1,12 @@
 use lib ('.', "../../Tools/perl_lib","perl_lib");
 use Cwd;
+use Cwd 'abs_path';
 use File::Path;
 use Getopt::Long;
 use Tools qw(InstallNameTool);
 
 my $root = getcwd();
+my $monoroot = abs_path($root."/../Mono");
 my $skipbuild=0;
 my $debug = 0;
 my $minimal = 0;
@@ -57,18 +59,14 @@ if (not $skipbuild)
 		$ENV{LDFLAGS} = "-m32";
 	}
 
+	chdir("$monoroot") eq 1 or die ("Failed chdir 2");
+
 	#this will fail on a fresh working copy, so don't die on it.
 	system("make distclean");
 	#were going to tell autogen to use a specific cache file, that we purposely remove before starting.
         #that way, autogen is forced to do all its config stuff again, which should make this buildscript
         #more robust if other targetplatforms have been built from this same workincopy
         system("rm linux.cache");
-
-	chdir("$root/eglib") eq 1 or die ("Failed chdir 1");
-	#this will fail on a fresh working copy, so don't die on it.
-	system("make distclean");
-	system("autoreconf -i") eq 0 or die ("Failed autoreconfing eglib");
-	chdir("$root") eq 1 or die ("Failed chdir 2");
 
 	system("autoreconf -i") eq 0 or die ("Failed autoreconfing mono");
 	my @autogenparams = ();
@@ -78,15 +76,6 @@ if (not $skipbuild)
 	unshift(@autogenparams, "--disable-nls");  #this removes the dependency on gettext package
 	unshift(@autogenparams, "--disable-parallel-mark");  #this causes crashes
 	unshift(@autogenparams, "--build=i686-pc-linux-gnu");  #Force x86 build
-
-	# From Massi: I was getting failures in install_name_tool about space
-	# for the commands being too small, and adding here things like
-	# $ENV{LDFLAGS} = '-headerpad_max_install_names' and
-	# $ENV{LDFLAGS} = '-headerpad=0x40000' did not help at all (and also
-	# adding them to our final gcc invocation to make the bundle).
-	# Lucas noticed that I was lacking a Mono prefix, and having a long
-	# one would give us space, so here is this silly looong prefix.
-	unshift(@autogenparams, "--prefix=/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890/1234567890");
 
 	if ($minimal)
 	{
@@ -107,16 +96,16 @@ mkpath($bintarget);
 mkpath($libtarget);
 
 print "Copying libmono.so\n";
-system("cp", "$root/mono/mini/.libs/libmono.so.0","$libtarget/libmono.so") eq 0 or die ("failed copying libmono.so.0");
+system("cp", "$monoroot/mono/mini/.libs/libmono-2.0.so.0","$libtarget/libmono.so") eq 0 or die ("failed copying libmono.so.0");
 
 print "Copying libmono.a\n";
-system("cp", "$root/mono/mini/.libs/libmono.a","$libtarget/libmono-static.a") eq 0 or die ("failed copying libmono.a");
+system("cp", "$monoroot/mono/mini/.libs/libmono-2.0.a","$libtarget/libmono-static.a") eq 0 or die ("failed copying libmono.a");
 
 if ($ENV{"UNITY_THISISABUILDMACHINE"})
 {
 	system("strip $libtarget/libmono.so") eq 0 or die("failed to strip libmono (shared)");
 }
 
-system("ln","-f","$root/mono/mini/mono","$bintarget/mono") eq 0 or die("failed symlinking mono executable");
-system("ln","-f","$root/mono/metadata/pedump","$bintarget/pedump") eq 0 or die("failed symlinking pedump executable");
+system("ln","-f","$monoroot/mono/mini/mono","$bintarget/mono") eq 0 or die("failed symlinking mono executable");
+system("ln","-f","$monoroot/mono/metadata/pedump","$bintarget/pedump") eq 0 or die("failed symlinking pedump executable");
 system("chmod","-R","755",$bintarget);
