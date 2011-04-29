db51d53e21ddbf95ac76be382589650a6d709f6e
Lucas Meijer <lucas@unity3d.com>
2011-04-29 10:56:06 +0200

    more xbuild tweaks

--- add_to_build_results/monodistribution/bin/cli.bat
+++ add_to_build_results/monodistribution/bin/cli.bat
@@ -1,6 +1,6 @@
 @echo off
 set MONO_PREFIX=%~dp0/..
 set MONO=%MONO_PREFIX%/bin/mono
-set MONO_PATH=%MONO_PREFIX%/lib/mono/2.0;%MONO_PREFIX%/lib/mono/3.5
+set MONO_PATH=%MONO_PREFIX%/lib/mono/3.5;%MONO_PREFIX%/lib/mono/2.0
 set MONO_CFG_DIR=%MONO_PREFIX%/etc
 "%MONO%" %*
--- add_to_build_results/monodistribution/bin/xbuild.bat
+++ add_to_build_results/monodistribution/bin/xbuild.bat
@@ -1,1 +1,1 @@
-@"%~dp0\mono.exe" %MONO_OPTIONS% "%~dp0\..\lib\mono\4.0\xbuild.exe" %*
+@"%~dp0\mono.exe" %MONO_OPTIONS% "%~dp0\..\lib\mono\3.5\xbuild.exe" %*
