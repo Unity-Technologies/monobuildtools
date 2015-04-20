19c451eef41f157cbeb1f6aa930eabc8046984ba
Levi Bard <levi@unity3d.com>
2015-04-20 18:20:59 +0200

    Return error codes from batch files.

--- add_to_build_results/monodistribution/bin/cli.bat
+++ add_to_build_results/monodistribution/bin/cli.bat
@@ -4,4 +4,5 @@ set MONO_PREFIX=%~dp0..
 set MONO=%MONO_PREFIX%\bin\mono
 set MONO_CFG_DIR=%MONO_PREFIX%\etc
 "%MONO%" %*
+exit /b %ERRORLEVEL%
 ENDLOCAL
--- add_to_build_results/monodistribution/bin/resgen2.bat
+++ add_to_build_results/monodistribution/bin/resgen2.bat
@@ -1,1 +1,2 @@
 @"%~dp0cli.bat" %MONO_OPTIONS% "%~dp0..\lib\mono\2.0\resgen.exe" %*
+exit /b %ERRORLEVEL%
