c911355b9da383e4109216b6040b0bce60be3b64
Mike Voorhees <michaelv@unity3d.com>
2016-02-01 13:20:30 -0500

    Change old build scripts to run using the new scripts

--- build_runtime_linux.pl
+++ build_runtime_linux.pl
@@ -1,165 +1,23 @@
-use strict;
-
-use lib ('.', "../../Tools/perl_lib","external/buildscripts/perl_lib");
 use Cwd;
 use Cwd 'abs_path';
-use File::Path;
 use Getopt::Long;
-use Tools qw(InstallNameTool);
+use File::Basename;
+use File::Path;
 
-my $root = getcwd();
-my $buildsroot = "$root/builds";
-my $buildir = $root;
-my $monoroot = $root;
+my $monoroot = File::Spec->rel2abs(dirname(__FILE__) . "/../..");
+my $monoroot = abs_path($monoroot);
+my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
-my $skipbuild=0;
-my $debug = 1;
-my $minimal = 0;
-my $cleanbuild = 1;
 my $build64 = 0;
-my $build_armel = 0;
-my $jobs = 8;
-
-my $teamcity=0;
-if ($ENV{UNITY_THISISABUILDMACHINE})
-{
-	print "rmtree-ing $buildsroot because we're on a buildserver, and want to make sure we don't include old artifacts\n";
-	rmtree($buildsroot);
-	$teamcity=1;
-} else {
-	print "not rmtree-ing $buildsroot, as we're not on a buildmachine";
-}
 
 GetOptions(
-   "skipbuild=i"=>\$skipbuild,
-   "debug=i"=>\$debug,
-   "minimal=i"=>\$minimal,
-   "cleanbuild=i"=>\$cleanbuild,
    "build64=i"=>\$build64,
-   "build-armel=i"=>\$build_armel,
-   "jobs=i"=>\$jobs,
 ) or die ("illegal cmdline options");
 
-die ("illegal cmdline options") if ($build64 and $build_armel);
-
-my $platform = $build64 ? 'linux64' : $build_armel ? 'linux-armel' : 'linux32' ;
-my $bintarget = "$buildsroot/monodistribution/bin-$platform";
-my $libtarget = "$buildsroot/embedruntimes/$platform";
-my $etctarget = "$buildsroot/monodistribution/etc-$platform";
-
-
-my $os = 'linux';
-my $arch = $build64 ? 'x86_64' : $build_armel ? 'armel' : 'i386' ;
-my $buildtarget = $buildir;
-my $cachefile = "$buildir/$os-$arch.cache";
-
-if ($minimal)
+my $arch32 = 1;
+if ($build64)
 {
-	$libtarget = "$buildsroot/embedruntimes/$platform-minimal";
+	$arch32 = 0;
 }
 
-print("libtarget: $libtarget\n");
-
-system("rm -f $bintarget/mono");
-system("rm -f $libtarget/libmono.so");
-system("rm -f $libtarget/libmono-static.a");
-system("rm -f $libtarget/libMonoPosixHelper.so");
-
-if (not $skipbuild)
-{
-	mkpath("$buildir");
-
-	my $archflags = '';
-
-	if (not $build64 and not $build_armel)
-	{
-		$archflags = '-m32';
-	}
-	if ($build_armel)
-	{
-		$archflags = '-marm -DARM_FPU_NONE';
-	}
-	if ($debug)
-	{
-		$ENV{CFLAGS} = "$archflags -g -O0";
-	} else
-	{
-		$ENV{CFLAGS} = "$archflags -Os";  #optimize for size
-	}
-	$ENV{CXXFLAGS} = $ENV{CFLAGS};
-	$ENV{LDFLAGS} = "$archflags";
-
-	# Nobody can remember why we were doing this, but it's faster to autogen when we need to
-	# chdir("$monoroot/eglib") eq 1 or die ("Failed chdir 1");
-	# system("autoreconf -i") eq 0 or die ("Failed autoreconfing eglib");
-
-	# chdir("$monoroot") eq 1 or die ("Failed chdir 2");
-	# system("autoreconf -i") eq 0 or die ("Failed autoreconfing mono");
-
-
-	my @autogenparams = ();
-	unshift(@autogenparams, "--cache-file=$cachefile");
-	unshift(@autogenparams, "--disable-mcs-build");
-	unshift(@autogenparams, "--with-glib=embedded");
-	unshift(@autogenparams, "--disable-nls");  #this removes the dependency on gettext package
-	unshift(@autogenparams, "--disable-parallel-mark");  #this causes crashes
-	if(not $build64 and not $build_armel)
-	{
-		unshift(@autogenparams, "--build=i686-pc-linux-gnu");  #Force x86 build
-	}
-
-	if ($minimal)
-	{
-		unshift(@autogenparams,"--enable-minimal=aot,logging,com,profiler,debug");
-	}
-
-	# Avoid "source directory already configured" ...
-	system('rm', '-f', 'config.status', 'eglib/config.status', 'libgc/config.status');
-
-	print("\n\n\n\nCalling configure with these parameters: ");
-	system("echo", @autogenparams);
-	print("\n\n\n\n\n");
-
-	chdir("$buildir") eq 1 or die ("Failed chdir 3");
-	system("calling $monoroot/configure",@autogenparams);
-	system("$monoroot/autogen.sh", @autogenparams) eq 0 or die ("failing configuring mono");
-
-	if ($cleanbuild == 1) {
-		system("rm $cachefile");
-		if (chdir("$buildtarget") eq 1) {
-			my $i;
-			foreach $i (qw(eglib libgc mono ikvm-native support))
-			{
-				print("make -C $i clean\n");
-				system('make', '-C', $i, 'clean');
-			}
-		}
-	}
-
-	system("make -j$jobs") eq 0 or die ("failing running make for mono");
-}
-
-mkpath($bintarget);
-mkpath($libtarget);
-mkpath("$etctarget/mono");
-
-print "Copying libmono.so\n";
-system("cp", "$buildtarget/mono/mini/.libs/libmonoboehm-2.0.so","$libtarget/libmono.so") eq 0 or die ("failed copying libmonoboehm-2.0.so");
-
-print "Copying libmono-static.a\n";
-system("cp", "$buildtarget/mono/mini/.libs/libmonoboehm-2.0.a","$libtarget/libmono-static.a") eq 0 or die ("failed copying libmonoboehm-2.0.a");
-
-print "Copying libMonoPosixHelper.so\n";
-system("cp", "$buildtarget/support/.libs/libMonoPosixHelper.so","$libtarget/libMonoPosixHelper.so") eq 0 or die ("failed copying libMonoPosixHelper.so");
-
-if ($ENV{"UNITY_THISISABUILDMACHINE"})
-{
-	system("strip $libtarget/libmono.so") eq 0 or die("failed to strip libmono (shared)");
-	system("strip $libtarget/libMonoPosixHelper.so") eq 0 or die("failed to strip libMonoPosixHelper (shared)");
-	system("echo \"mono-runtime-$platform = $ENV{'BUILD_VCS_NUMBER_mono_unity_2_10_2'}\" > $buildsroot/versions.txt");
-}
-
-system("ln","-f","$buildtarget/mono/mini/mono-boehm","$bintarget/mono") eq 0 or die("failed symlinking mono executable");
-system("ln","-f","$buildtarget/mono/metadata/pedump","$bintarget/pedump") eq 0 or die("failed symlinking pedump executable");
-system('cp',"$buildtarget/data/config","$etctarget/mono/config");
-system("chmod","-R","755",$bintarget);
+system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--test=1", "-artifact=1", "--arch32=$arch32", "--classlibtests=0", "disablemcs=1") eq 0 or die ("Failed builidng mono\n");
--- build_runtime_win64.pl
+++ build_runtime_win64.pl
@@ -1,71 +1,12 @@
-sub CompileVCProj;
+use Cwd;
 use Cwd 'abs_path';
-use File::Spec;
+use Getopt::Long;
 use File::Basename;
-use File::Copy;
 use File::Path;
-my $root = File::Spec->rel2abs(dirname($0) . '/../..');
-my $monoroot = $root;
-my $buildsroot = "$root/builds";
-my $buildir = $root;
 
-if ($ENV{UNITY_THISISABUILDMACHINE})
-{
-	print "rmtree-ing $root/builds because we're on a buildserver, and want to make sure we don't include old artifacts\n";
-	rmtree("$root/builds");
-} else {
-	print "not rmtree-ing $root/builds, as we're not on a buildmachine";
-}
+my $monoroot = File::Spec->rel2abs(dirname(__FILE__) . "/../..");
+my $monoroot = abs_path($monoroot);
+my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
-CompileVCProj("$monoroot/msvc/mono.sln","Release|x64",0);
-
-my $remove = "$buildsroot/embedruntimes/win32/libmono.bsc";
-if (-e $remove)
-{
-	unlink($remove) or die("can't delete libmono.bsc");
-}
-
-
-#have a duplicate for now...
-copy("$buildsroot/embedruntimes/win64/mono.dll","$buildsroot/monodistribution/bin-x64/mono.dll");
-copy("$buildsroot/embedruntimes/win64/mono.pdb","$buildsroot/monodistribution/bin-x64/mono.pdb");
-
-if ($ENV{UNITY_THISISABUILDMACHINE})
-{
-	system("echo mono-runtime-win32 = $ENV{'BUILD_VCS_NUMBER'} > $buildsrootwin\\versions.txt");
-}
-
-sub CompileVCProj
-{
-	my $sln = shift(@_);
-	my $slnconfig = shift(@_);
-	my $incremental = shift(@_);
-	my $projectname = shift(@_);
-	my @optional = @_;
-
-
-	my @devenvlocations = ($ENV{"PROGRAMFILES(X86)"}."/Microsoft Visual Studio 10.0/Common7/IDE/devenv.com",
-		       "$ENV{PROGRAMFILES}/Microsoft Visual Studio 10.0/Common7/IDE/devenv.com",
-		       "$ENV{REALVSPATH}/Common7/IDE/devenv.com");
-
-	my $devenv;
-	foreach my $devenvoption (@devenvlocations)
-	{
-		if (-e $devenvoption) {
-			$devenv = $devenvoption;
-		}
-	}
-
-	my $buildcmd = $incremental ? "/build" : "/rebuild";
-
-        if (defined $projectname)
-        {
-            print "devenv.exe $sln $buildcmd $slnconfig /project $projectname @optional \n\n";
-            system($devenv, $sln, $buildcmd, $slnconfig, '/project', $projectname, @optional) eq 0
-                    or die("VisualStudio failed to build $sln");
-        } else {
-            print "devenv.exe $sln $buildcmd $slnconfig\n\n";
-            system($devenv, $sln, $buildcmd, $slnconfig) eq 0
-                    or die("VisualStudio failed to build $sln");
-        }
-}
+# TODO by Mike : Pass along predefined mono and cygwin install locations
+system("perl", "$buildScriptsRoot/build_win_wrapper.pl", "--build=1", "--clean=1", "--test=1", "-artifact=1", "--classlibtests=0", "disablemcs=1") eq 0 or die ("Failed builidng mono\n");
