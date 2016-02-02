c4ac8e4e6147468694aa989dbc9a0be6377c5e94
Mike Voorhees <michaelv@unity3d.com>
2016-02-02 14:46:25 -0500

    Change katana facing build scripts to use the new checked in mono build deps

--- build_runtime_win64.pl
+++ build_runtime_win64.pl
@@ -9,4 +9,4 @@ my $monoroot = abs_path($monoroot);
 my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
 # TODO by Mike : Pass along predefined mono and cygwin install locations
-system("perl", "$buildScriptsRoot/build_win_wrapper.pl", "--build=1", "--clean=1", "--test=1", "--artifact=1", "--classlibtests=0") eq 0 or die ("Failed builidng mono\n");
+system("perl", "$buildScriptsRoot/build_win_wrapper.pl", "--build=1", "--clean=1", "--test=1", "--artifact=1", "--classlibtests=0", "--forcedefaultbuilddeps=1", "--checkoutonthefly=1") eq 0 or die ("Failed builidng mono\n");
