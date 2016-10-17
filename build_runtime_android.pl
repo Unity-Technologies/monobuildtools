d3232d449dcfb7576cdd73631adc68008a8928ed
Mike Voorhees <michaelv@unity3d.com>
2016-10-17 14:45:11 -0400

    Update android  runtime build scripts to update all the arm variations we care about

--- build_runtime_android.pl
+++ build_runtime_android.pl
@@ -8,17 +8,20 @@ my $monoroot = File::Spec->rel2abs(dirname(__FILE__) . "/../..");
 my $monoroot = abs_path($monoroot);
 my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
-my $build64 = 0;
+my $androidArch = "";
 
 GetOptions(
-   "build64=i"=>\$build64,
+   "androidarch=i"=>\$androidArch,
 ) or die ("illegal cmdline options");
 
-my $arch32 = 1;
-if ($build64)
+# By default, build runtime for all the variants we need.  But allow something to specify an individual variation to build
+if ($androidArch eq "")
 {
-	$arch32 = 0;
+	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=1", "--android=1", "--androidarch=armv5", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono for armv5\n");
+	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=1", "--android=1", "--androidarch=armv6_vfp", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono for armv6_vfp\n");
+	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=1", "--android=1", "--androidarch=armv7a", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono for armv7a\n");
 }
-
-
-system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=$arch32", "--android=1", "--androidarch=armv5", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
+else
+{
+	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=1", "--android=1", "--androidarch=$androidArch", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono for $androidArch\n");
+}
