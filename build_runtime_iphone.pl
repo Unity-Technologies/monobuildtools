5f441f9f4643077dcf9681e8a6609330c843c5b8
Mike Voorhees <michaelv@unity3d.com>
2016-11-02 10:55:26 -0400

    Need to build our own mono first to use for bootstrapping because 32bit slice is missing from build deps mono

--- build_runtime_iphone.pl
+++ build_runtime_iphone.pl
@@ -21,25 +21,44 @@ GetOptions(
 ) or die ("illegal cmdline options");
 
 # Build everything by default
-if (!$runtime && !$xcomp && !simulator)
+if ($runtime == 0 && $xcomp == 0 && $simulator == 0)
 {
+	print ">>> All iphone related builds will be ran\n";
 	$runtime = 1;
 	$xcomp = 1;
 	$simulator = 1;
 }
 
-
 if ($runtime)
 {
+	print ">>> Building iphone runtime\n";
 	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=$clean", "--artifact=1", "--arch32=1", "--iphoneArch=armv7", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono for iphone\n");
 }
 
 if ($xcomp)
 {
+	# TODO : This is a horrible waste of time, and we should fix it, but for now it gets things working.
+	# The mono we have in the build deps for bootstrapping doesn't have a 32bit slice, which we need in order to run the MonoAotOffsetsDumper.
+	# To get around this for the moment, we'll build the runtime & classlibs first, and then use that to run the MonoAotOffsetsDumper.
+	# Once we update the mono in the build deps, we can remove this.
+	if(!(-f "$monoroot/builds/monodistribution/bin/mono"))
+	{
+		print ">>> Building mono to use for bootstrapping.  The version in mono build deps is missing the 32bit slice and we need a 32bit version to run the MonoAotOffsetsDumper\n";
+		#system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--arch32=1", "--clean=$clean", "--classlibtests=0", "--artifact=1", "--artifactscommon=1", "--forcedefaultbuilddeps=1") eq 0 or die ("failing building mono 32bit for bootstrapping\n");
+		#system("perl", "$buildScriptsRoot/build_all_osx.pl", "--build=1", "--artifact=1", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
+		system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--arch32=1", "--artifactscommon=1", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
+
+		system("cp", "$monoroot/builds/monodistribution/bin-osx-tmp-i386/mono", "$monoroot/builds/monodistribution/bin/.") eq 0 or die ("failed to copy mono over from bin-osx-tmp-i386 to bin\n");
+		system("chmod", "+x", "$monoroot/builds/monodistribution/bin/mono") eq 0 or die("Failed to chmod mono\n");
+
+	}
+
+	print ">>> Building iphone cross compiler\n";
 	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=$clean", "--artifact=1", "--arch32=1", "--iphonecross=1", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng iphone cross compiler\n");
 }
 
 if ($simulator)
 {
+	print ">>> Building iphone simulator\n";
 	system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=$clean", "--artifact=1", "--arch32=1", "--iphonesimulator=1", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng iphone simulator\n");
 }
