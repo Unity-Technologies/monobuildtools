406c580f8e483ebcf27190939c58563a6b3a646c
Mike Voorhees <michaelv@unity3d.com>
2016-11-16 20:38:51 -0500

    Build the AOT profile as part of the classless build

--- build_classlibs_osx.pl
+++ build_classlibs_osx.pl
@@ -8,4 +8,14 @@ my $monoroot = File::Spec->rel2abs(dirname(__FILE__) . "/../..");
 my $monoroot = abs_path($monoroot);
 my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
-system("perl", "$buildScriptsRoot/build.pl", "--build=1", "--clean=1", "--artifact=1", "--artifactscommon=1", "--buildusandboo=1", "--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
+system(
+	"perl",
+	"$buildScriptsRoot/build.pl",
+	"--build=1",
+	"--clean=1",
+	"--artifact=1",
+	"--artifactscommon=1",
+	"--aotprofile=mobile_static",
+	"--aotprofiledestname=unity_aot",
+	"--buildusandboo=1",
+	"--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
