497e722b1883458e6a4db424c3b867366b0b6866
Mike Voorhees <michaelv@unity3d.com>
2016-11-17 14:55:59 -0500

    Add some extra arg handling

--- build_classlibs_osx.pl
+++ build_classlibs_osx.pl
@@ -8,14 +8,27 @@ my $monoroot = File::Spec->rel2abs(dirname(__FILE__) . "/../..");
 my $monoroot = abs_path($monoroot);
 my $buildScriptsRoot = "$monoroot/external/buildscripts";
 
+my $build = 1;
+my $clean = 1;
+
+# Handy troubleshooting/niche options
+my $shortPrefix = 0;
+
+GetOptions(
+   "build=i"=>\$build,
+   "clean=i"=>\$clean,
+   'shortprefix=i'=>\$shortPrefix,
+) or die ("illegal cmdline options");
+
 system(
 	"perl",
 	"$buildScriptsRoot/build.pl",
-	"--build=1",
-	"--clean=1",
+	"--build=$build",
+	"--clean=$clean",
 	"--artifact=1",
 	"--artifactscommon=1",
 	"--aotprofile=mobile_static",
 	"--aotprofiledestname=unity_aot",
 	"--buildusandboo=1",
-	"--forcedefaultbuilddeps=1") eq 0 or die ("Failed builidng mono\n");
+	"--forcedefaultbuilddeps=1",
+	"--shortprefix=$shortPrefix") eq 0 or die ("Failed builidng mono\n");
